1bad2f492796c1655569c716a0958697963adb17
isabela porto <isamporto@hotmail.com>
2020-09-28 21:29:15 -0300

    Fixed some eslint problems and added predeploy

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -6,7 +6,6 @@ import Message from '../components/message';
 import MessageForm from './message_form';
 import { fetchMessages } from '../actions';
 
-
 class MessageList extends Component {
   componentWillMount() {
     this.fetchMessages();
@@ -16,29 +15,31 @@ class MessageList extends Component {
     this.refresher = setInterval(this.fetchMessages, 1000);
   }
 
-  componentWillUnmount() {
-    clearInterval(this.refresher);
-  }
-
   componentDidUpdate() {
     this.list.scrollTop = this.list.scrollHeight;
   }
 
-  fetchMessages = () => {
-    const { selectedChannel } = this.props;
-    this.props.fetchMessages(selectedChannel);
+  componentWillUnmount() {
+    clearInterval(this.refresher);
   }
 
+  fetchMessages = () => {
+    const { selectedChannel, fetchMessages } = this.props;
+    fetchMessages(selectedChannel);
+  }
 
   render() {
     const { messages, selectedChannel } = this.props;
     return (
       <div className="messages-container">
         <div className="title">
-          <span>Channel #{selectedChannel}</span>
+          <span>
+            Channel #
+            {selectedChannel}
+          </span>
         </div>
         <div className="message-list" ref={(list) => { this.list = list; }}>
-          {messages.map(message => <Message message={message} key={message.id} />)}
+          {messages.map((message) => <Message message={message} key={message.id} />)}
         </div>
         <MessageForm />
       </div>
